6aafcfc127a39974e3c361a01301ec1b3b6a7f64
Huy Hong <t.h.hong@student.tue.nl>
2026-04-26 00:20:10 +0200

    Client authentication

---
+++ auth.py
@@ -0,0 +1,21 @@
+import spotipy
+import os
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+
+def auth_client():
+    """
+    Authenticate a Spotify client.
+    """
+    load_dotenv(verbose = True)
+    CLIENT_ID = os.getenv("CLIENT_ID")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    REDIRECT_URI = os.getenv("REDIRECT_URI")
+    SCOPE = os.getenv("SCOPE")
+    authentication_manager = SpotifyOAuth(client_id = CLIENT_ID, 
+                                        client_secret = CLIENT_SECRET,
+                                        redirect_uri = REDIRECT_URI,
+                                        scope = SCOPE)
+    authenticated_client = spotipy.Spotify(auth_manager = authentication_manager)
+
+    return authenticated_client
---
+++ main.py
@@ -0,0 +1,9 @@
+from auth import *
+from playlist import *
+
+def main():
+    None
+
+
+if __name__ == "__main__":
+    main()
