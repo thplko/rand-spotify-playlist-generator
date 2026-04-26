1277de54d477960b2da0accfe4cc7c68abb28d18
Huy Hong <t.h.hong@student.tue.nl>
2026-04-27 01:30:33 +0200

    Started on command-line arguments for the program

--- main.py
+++ main.py
@@ -1,9 +1,74 @@
-from auth import *
+import sys
+import random as rand
+from spotipy.oauth2 import SpotifyOauthError
+from auth import auth_client
 from playlist import *
+from parser import parse_arguments
 
 def main():
-    None
+    args = parse_arguments()
 
+    print(f"Authenticating...")
+    try:
+        client = auth_client()
+        print("Authentication successful.")
+    except SpotifyOauthError:
+        print("Authentication failed, please check that your client ID and secret are correct.")
+        sys.exit(1)
+
+    # Checking arguments
+    if args.list_playlists:
+        name_playlists(client)
+        sys.exit(1)
+
+    if args.list_tracks:
+        if not args.playlist_ids:
+            print("At least one playlist ID is required.")
+        for playlist in args.playlist_ids:
+            name_tracks(client, playlist, args.limit, args.offset)
+        sys.exit(0)
+
+    if not args.playlist_ids:
+        print("At least one playlist ID is required.")
+        sys.exit(1)
+
+    if not args.limit:
+        print(f"The default limit used is {len(track_uris)}.")
+    if not args.offset:
+        print("The default offset used is 0.")
+    if not args.shuffle:
+        print("The playlist is not shuffled.")
+    if not args.no_shuffle:
+        print("The playlist is shuffled.")
+
+    # Begin song selection
+    print(f"Selecting songs...")
+    print(f"Playlist IDs: {args.playlist_ids}")
+    print(f"Limit per playlist: {args.limit}")
+    print(f"Offset per playlist: {args.offset}")
+    
+    track_uris = []
+    for playlist in args.playlist_ids:
+        uris = get_tracks(client, playlist, args.limit, args.offset, args.shuffle)
+        track_uris.extend(uris)
+
+    if not track_uris:
+        print("No tracks were retrieved. Program exitting.")
+        sys.exit(1)
+
+    print(f"Total tracks selected: {len(track_uris)}")
+
+    # Begin playlist generation
+    playlist_name = input("Enter name for the new playlist: ").strip()    
+    if not playlist_name:
+        playlist_name = "New Playlist"
+    status = input("Make playlist public? (y/n): ").strip().lower() == 'y'
+    collab = input("Make playlist collaborative? (y/n): ").strip().lower() == 'y'
+    desc = input("Enter playlist description: ").strip().lower()
+
+    generate_playlist(client, track_uris, playlist_name, status, collab, desc)
+    print("Playlist generation completed!")    
+    sys.exit(1)
 
 if __name__ == "__main__":
     main()
---
+++ parser.py
@@ -0,0 +1,51 @@
+import argparse
+
+def parse_arguments():
+    """
+    Creating command-line arguments for using the program.
+    """
+    parser = argparse.ArgumentParser(
+        prog = 'RandomSpotifyGenerator',
+        description = "Generate new playlists with randomly selected songs from existing Spotify playlists.",    
+    )
+    parser.add_argument(
+        "--playlist-ids",
+        type = str,
+        nargs = '+',
+        help = "One or more Spotify playlist IDs."
+    )
+    parser.add_argument(
+        "--limit", 
+        type = int,
+        help = "Number of songs taken from selected playlists."
+    )
+    parser.add_argument(
+        "--offset", 
+        type = int, 
+        default = 0,
+        help = "Number of songs skipped in the playlist upon selection."
+    )
+    parser.add_argument(
+        "--list-tracks",
+        action = "store_true",
+        help = "List the names and URIs of all your tracks in a given Spotify playlist."
+    )
+    parser.add_argument(
+        "--list-playlists",
+        action = "store_true",
+        help = "List the names and IDs of all your Spotify playlists."
+    )
+    parser.add_argument(
+        "--shuffle",
+        action = "store_true",
+        default = True,
+        help = "Spotify playlist is shuffled."
+    )
+    parser.add_argument(
+        "--no-shuffle",
+        dest = "shuffle",
+        action = "store_false",
+        help = "Spotify playlist is not shuffled."
+    )
+
+    return parser.parse_args()
